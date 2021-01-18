6f077d7e690051d3f2edf0f5aa586227fc160c8f
Ben Galloway <bdgalloway85@gmail.com>
2021-01-17 20:43:28 -0600

    add server and routes skeleton

---
+++ routes/index.js
@@ -0,0 +1,8 @@
+const router = express.Router();
+const apiRoutes = require('./api');
+const htmlRoutes = require('./html');
+
+router.use('/api', apiRoutes);
+router.use('/', htmlRoutes);
+
+module.exports = router;
