a883396d99b769ff12c9bdad49cbfb4114ed534c
Ben Galloway <bdgalloway85@gmail.com>
2021-01-17 21:37:03 -0600

    api routes started

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
-// const router = require('express').Router;
+const router = require('express').Router();
 
-// router.get('/', (_req, res) => {
-// res.send('index.html');
+router.get('/', (_req, res) => {
+res.send('<h1>api response<h1>');
 
 
-// });
+});
+
+module.exports = router;
