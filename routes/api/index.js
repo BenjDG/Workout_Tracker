9086910231b163b7759564210143154f9212ad4a
Ben Galloway <bdgalloway85@gmail.com>
2021-01-18 00:51:24 -0600

    api get workouts route

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,32 @@
 const router = require('express').Router();
+const db = require('../../models');
 
-router.get('/', (_req, res) => {
-res.send('<h1>api response<h1>');
 
+
+router.post('/workouts', (req, res) => {
+console.log(req.body);
+// res.send('<h1>api response<h1>');
 
 });
+
+// GET /workouts
+router.get('/workouts', (_req, res) => {
+
+  db.Workout.find({})
+  .then(r => {
+    console.log(r);
+    console.log(r[0]);
+    console.log(r[0].exercises[0].duration);
+  res.status(400).json(r);
+  })
+});
+
+// PUT /workouts:id
+
+// POST /workouts
+
+// GET /workouts/range
+
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
@@ -9,6 +10,8 @@ app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 
 app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.use(routes);
 
