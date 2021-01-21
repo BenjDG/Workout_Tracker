d4fc57f7de47702f15b6a8f04894c84491737919
Ben Galloway <bdgalloway85@gmail.com>
2021-01-21 00:06:27 -0600

    change title of graph on stats page

--- public/stats.js
+++ public/stats.js
@@ -150,7 +150,7 @@ function populateChart (data) {
       labels: workouts,
       datasets: [
         {
-          label: 'Excercises Performed',
+          label: 'Duration of Excercises Performed',
           backgroundColor: colors,
           data: durations
         }
@@ -159,7 +159,7 @@ function populateChart (data) {
     options: {
       title: {
         display: true,
-        text: 'Excercises Performed'
+        text: 'Duration of Excercises Performed'
       }
     }
   });
@@ -170,7 +170,7 @@ function populateChart (data) {
       labels: workouts,
       datasets: [
         {
-          label: 'Excercises Performed',
+          label: 'Weight of Excercises Performed',
           backgroundColor: colors,
           data: pounds
         }
@@ -179,7 +179,7 @@ function populateChart (data) {
     options: {
       title: {
         display: true,
-        text: 'Excercises Performed'
+        text: 'Weight of Excercises Performed'
       }
     }
   });
